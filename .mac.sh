2e853eddef94a1efc2fbdcf460a5d2720306fec4
Cleyson Braga de Oliveira <Cleysonb.Oliveira@gmail.com>
2023-04-19 20:09:47 -0300

    Adding script to allow macos automation

--- .mac.sh
+++ .mac.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-if [ "$(uname)" = "Darwin" ]; then
-	./.mac.sh
-	exit 0
-fi;
 
 libDir="Marketplace/libs"
 package="DevTools.jar"
 sourcePath="$(find "src" -type f -name "*.java")"
-sourcePath="${sourcePath//[$'\t\r\n']/' '}"
+sourcePath="${sourcePath//[$'\t\r\n']/ ./}"
 
 rm -rf bin ; mkdir bin
 rm -rf result
@@ -18,9 +14,9 @@ javac $sourcePath -d bin
 
 if [ $? -eq 0 ]; then
 	classPath="$(find "bin" -type f -name "*.class")"
-	classPath="${classPath//[$'\t\r\n']/' '}"
-	classPath="${classPath//'bin/'/' '}"
- 
+	classPath="${classPath//[$'\t\r\n']/ }"
+	classPath="${classPath//bin\// }"
+
 	cd ./bin
 		jar cfmv ../$package ../META-INF/MANIFEST.MF $classPath
 	cd ..
