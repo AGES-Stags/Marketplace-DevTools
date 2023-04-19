e6c46dd2eb283a8510a52dd25d0a1921546b50e2
Cleyson Braga de Oliveira <Cleysonb.Oliveira@gmail.com>
2023-04-18 23:51:28 -0300

    Fixed library export and import issues

--- src/inject/Mockup.java
+++ src/inject/Mockup.java
@@ -8,20 +8,9 @@ import helper.FileManager;
 public class Mockup {
 
 	private static FileManager fileManager;
-	static {
-		try {
-			fileManager = new FileManager("config.yaml");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
-	public static void main(String args[]) {
-
-	}
-
-	public static void test() {
-		System.out.println("test");
+	public static void sout() {
+		System.out.println("Print from the library");
 	}
 
 	public static void products(Class<Object> listClass, Class<Object> productClass)
